e891de361db3e5694b3c2b9e0d4d1c027386bd9f
houin679 <83742598+houin679@users.noreply.github.com>
2023-06-23 11:50:38 +0800

    Update comment.js

--- comment.js
+++ comment.js
@@ -1,4 +1,89 @@
-  function createCommentElement(comment) {
+$(document).ready(function() {
+  // Create comment element
+  var comment = $('<div>', {
+    class: 'comment row'
+  });
+
+  var ratingBar = $('<div>', {
+    class: 'rating_bar'
+  });
+
+  var upvoteButton = $('<div>', {
+    class: 'button upvote',
+    text: '⮝'
+  });
+
+  var counter = $('<div>', {
+    class: 'counter',
+    text: '0'
+  });
+
+  var downvoteButton = $('<div>', {
+    class: 'button downvote',
+    text: '⮟'
+  });
+
+  ratingBar.append(upvoteButton, counter, downvoteButton);
+
+ 
+  var info = $('<div>', {
+    class: 'info'
+  });
+
+  
+  var text = $('<div>', {
+    class: 'text',
+    text: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa'
+  });
+
+
+  var author = $('<div>', {
+    class: 'author',
+    text: '@Foo.Bar'
+  });
+
+ 
+  var tools = $('<div>', {
+    class: 'tools'
+  });
+
+
+  var editButton = $('<button>', {
+    class: 'edit-comment',
+    text: 'Edit'
+  });
+
+ 
+  var deleteButton = $('<button>', {
+    class: 'delete-comment',
+    text: 'Delete'
+  });
+
+  tools.append(editButton, deleteButton);
+
+  
+  info.append(text, author, tools);
+
+  
+  comment.append(ratingBar, info);
+
+ 
+  $('.comment_container').append(comment);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  function createCommentElement(comment) {
                 var commentItem = $('<li>', {
                     class: 'comment'
                 });
@@ -60,3 +145,5 @@
 
                 postContainer.append(comment);
             }
+
+*/
