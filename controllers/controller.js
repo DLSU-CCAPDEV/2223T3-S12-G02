9c7840af124aeebf9181a908220dd9d5b55aa5ef
Aian Fuentespina <aian_fuentespina@dlsu.edu.ph>
2023-07-21 10:48:27 +0800

    edited routes and controllers. HBS files were updated to use public folder.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,37 @@
 const controller = {
 
-    
+    getRoot: function(req, res) {
+        res.render(`home_guest`);
+    },
+
+    redirectRoot: function(req, res) {
+        res.redirect(`/`);
+    },
+
+    // TODO: check if user logged in
+    getRootLogged: function(req, res) {
+        res.render(`home`);
+    },
+
+    getSearch: function(req, res) {
+        res.render(`search`);
+    },
+
+    getLogin: function(req, res) {
+        res.render(`login`);
+    },
+
+    getRegister: function(req, res) {
+        res.render(`register`);
+    },
+
+    getPost: function(req, res) {
+        res.render(`post`);
+    },
+
+    getProfile: function(req, res) {
+        res.render(`profile`);
+    }
 
     /* // Reference from original code //
     getRoot: function(req, res) {
