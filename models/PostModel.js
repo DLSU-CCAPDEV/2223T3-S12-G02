6e0bb2bc346362f660fea37029e6334f71a75dd5
houin679 <83742598+houin679@users.noreply.github.com>
2023-07-23 17:20:23 +0800

    Update PostModel.js

--- models/PostModel.js
+++ models/PostModel.js
@@ -24,19 +24,14 @@ var PostSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    postLikes: {
-    type: Number,
-    required: true,
-  },
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }], // Array of comment references
-});
-   /* 
+    
+ 
     postLikes: {
         type: Number,
         required: true
     }
 
 });
-*/
+
 
 module.exports = mongoose.model('Post', PostSchema);
