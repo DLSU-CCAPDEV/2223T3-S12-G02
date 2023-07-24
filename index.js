6054390bb14dc880bf5d6588755c8d6cd519d598
Aian Fuentespina <aian_fuentespina@dlsu.edu.ph>
2023-07-24 16:48:44 +0800

    Sessions fixed

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hbs = require(`hbs`);
 const session = require('express-session');
 const routes = require('./routes/routes.js');
 const db = require(`./models/db.js`);
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 // Set-up Express
 const app = express();
@@ -17,12 +18,19 @@ hostname = process.env.HOSTNAME;
 // connect to database
 db.connect();
 
+// store for session
+const store = new MongoDBStore({
+    uri: 'mongodb+srv://aianfuentespina:AojZtvwgYY0QOS91@cluster0.45z065t.mongodb.net/?retryWrites=true&w=majority', // Your MongoDB connection URI
+    collection: 'sessions', // Collection name to store sessions
+});
+
 // setup sessions system
 app.use(
     session({
         secret: 'Your_Secret_Key',
         resave: false,
         saveUninitialized: false,
+        store: store,
         cookie: { maxAge: 60 * 60 * 1000 }, // Session duration in milliseconds (e.g., 1 hour)
     })
 );
